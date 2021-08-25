1872be418ebe2795e17965950188d78ec03dd3fc
Johnnyboi3020 <joaovitorzampier09@gmail.com>
2021-08-25 13:26:07 -0300

    Adicionar arquivos de projeto.

---
+++ MetodoNota/Ingresso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetodoNota
+{
+    class Ingresso
+    {
+        private double Valor { get; set; }
+        public Ingresso(double v)
+        {
+            Valor = v;
+        }
+        public override string ToString()
+        {
+            return $"O valor do ingresso é: {Valor}";
+        }
+    }
+}
---
+++ MetodoNota/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MetodoNota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o valor do ingresso escolhido?");
+            double x = double.Parse(Console.ReadLine());
+
+            Ingresso i = new Ingresso(x);
+
+            Console.WriteLine(i);
+
+            Console.ReadLine();
+        }
+    }
+}
